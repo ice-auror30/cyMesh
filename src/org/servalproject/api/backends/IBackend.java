4ed82119d39485c2a23364252a213827433cd653
Matt Gerst <mattgerst@gmail.com>
2016-04-19 14:35:18 -0500

    Created the IBackend.

---
+++ src/org/servalproject/api/backends/IBackend.java
@@ -0,0 +1,37 @@
+package org.servalproject.api.backends;
+
+import org.servalproject.servald.Peer;
+
+/**
+ * Defines a backend for the NetworkAPI to use. IBackends are intended to
+ * be used to provide the ability to switch out the underlying protocols
+ * being used by the NetworkAPI. A backend should provide all necessary
+ * methods to send generic data.
+ */
+public interface IBackend {
+    /**
+     * Send arbitrary bytes to another Peer. Protocol specific things
+     * should be implemented on top of this method.
+     *
+     * @param dst The peer to send to
+     * @param data The data to send
+     * @return true if successful, false otherwise
+     */
+    boolean sendBytes(Peer dst, byte[] data);
+
+    /**
+     * Send MSG_PING to the specific peer.
+     *
+     * @param dst The peer to ping
+     */
+    void sendPing(Peer dst);
+
+    /**
+     * Send MSG_PONG to the specified peer.
+     *
+     * Should only be sent after receiving MSG_PING.
+     *
+     * @param dst The peer to pong
+     */
+    void sendPong(Peer dst);
+}
