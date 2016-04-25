94932802985aeb76719815f09731f0ebbd8e332e
Adit Kushare <adit@iastate.edu>
2016-04-25 18:54:40 -0500

    commenting out code thats throwing compile errors

--- src/org/servalproject/services/intentListener.java
+++ src/org/servalproject/services/intentListener.java
@@ -1,30 +1,30 @@
 
 
 /**
- * Created by Adit
- */
-package org.servalproject.sensors;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-
-
-public class intentListener extends BroadcastReceiver
-{
-    // Get the request to fire off the intent
-
-    public void onReceive(Context context, Intent intent) {
-
-        //Todo based on team discussion
-        //Process the intent using the string supplied in the intent
-        String request = "";
-        try {
-
-            intentService.getInstance().processRequest(request);
-
-        }
-        catch (Exception e) {
-          e.printStackTrace();
-        }
-    }
-}
+// * Created by Adit
+// */
+//package org.servalproject.sensors;
+//import android.content.BroadcastReceiver;
+//import android.content.Context;
+//import android.content.Intent;
+//
+//
+//public class intentListener extends BroadcastReceiver
+//{
+//    // Get the request to fire off the intent
+//
+//    public void onReceive(Context context, Intent intent) {
+//
+//        //Todo based on team discussion
+//        //Process the intent using the string supplied in the intent
+//        String request = "";
+//        try {
+//
+//            intentService.getInstance().processRequest(request);
+//
+//        }
+//        catch (Exception e) {
+//          e.printStackTrace();
+//        }
+//    }
+//}
