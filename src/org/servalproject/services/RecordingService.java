68f9514ca0a10a1da1ec63dc45acaa5bd73cba97
Brad Anson <banson@iastate.edu>
2016-10-10 16:46:05 -0500

    Improving Camera Quality

--- src/org/servalproject/services/RecordingService.java
+++ src/org/servalproject/services/RecordingService.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.graphics.SurfaceTexture;
+import android.media.CamcorderProfile;
 import android.media.MediaRecorder;
 import android.os.Environment;
 import android.os.IBinder;
@@ -41,13 +42,9 @@ public class RecordingService extends Service implements SurfaceHolder.Callback 
         holder = cameraView.getHolder();
         holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
         holder.addCallback(this);
-        WindowManager wm = (WindowManager)getApplicationContext()
-                .getSystemService(Context.WINDOW_SERVICE);
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
-                1, 1, //Must be at least 1x1
-                WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
-                0,
-                PixelFormat.UNKNOWN);
+        WindowManager wm = (WindowManager)getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(1, 1,
+                WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY, 0, PixelFormat.UNKNOWN);
         wm.addView(cameraView, params);
 
         startStopRecording(SIDString);
@@ -58,15 +55,33 @@ public class RecordingService extends Service implements SurfaceHolder.Callback 
         recorder = new MediaRecorder();
         recorder.setAudioSource(MediaRecorder.AudioSource.DEFAULT);
         recorder.setVideoSource(MediaRecorder.VideoSource.DEFAULT);
-        recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-        recorder.setVideoEncoder(MediaRecorder.VideoEncoder.DEFAULT);
-        recorder.setVideoEncodingBitRate(3000000);
         recorder.setOrientationHint(90);
 
-       /* CamcorderProfile cpHigh = CamcorderProfile
-                .get(CamcorderProfile.QUALITY_HIGH);
-        recorder.setProfile(cpHigh);*/
+        CamcorderProfile profile = null;
+
+        if (CamcorderProfile.hasProfile(CamcorderProfile.QUALITY_1080P)) {
+            profile = CamcorderProfile.get(CamcorderProfile.QUALITY_1080P);
+            Log.d(TAG, "Profile set to 1080");
+        }else if (CamcorderProfile.hasProfile(CamcorderProfile.QUALITY_720P)) {
+            Log.d(TAG, "Profile set to 720");
+            profile = CamcorderProfile.get(CamcorderProfile.QUALITY_720P);
+        }else if(CamcorderProfile.hasProfile(CamcorderProfile.QUALITY_480P)){
+            Log.d(TAG, "Profile set to 480");
+            profile=CamcorderProfile.get(CamcorderProfile.QUALITY_480P);
+        }else if (CamcorderProfile.hasProfile(CamcorderProfile.QUALITY_HIGH)) {
+            profile = CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH);
+            Log.d(TAG, "Profile set to HIGH");
+        }
+        if (profile != null){
+            recorder.setProfile(profile);
+        } else {
+            Log.d(TAG, "Profile set to NULL");
+            recorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            recorder.setVideoEncoder(MediaRecorder.VideoEncoder.DEFAULT);
+            recorder.setVideoEncodingBitRate(3000000);
+        }
+
         Log.d(TAG, "initRecorder finished");
     }
 
@@ -120,37 +135,19 @@ public class RecordingService extends Service implements SurfaceHolder.Callback 
         }
     }
 
-    public void surfaceCreated(SurfaceHolder holder) {
-        //cameraView = Main.getCameraSurface();
-        //holder = cameraView.getHolder();
-        //holder.addCallback(this);
-    }
+    public void surfaceCreated(SurfaceHolder holder) {}
 
     public void surfaceChanged(SurfaceHolder holder, int format, int width,
-                               int height) {
-        Log.d("Camera","Surface changed");
-    }
+                               int height) {}
 
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        if (recording) {
-            recorder.stop();
-            recording = false;
-        }
-        recorder.release();
-        recorder = new MediaRecorder();
-        initRecorder();
-        holder = cameraView.getHolder();
-        holder.addCallback(this);
-    }
+    public void surfaceDestroyed(SurfaceHolder holder) {}
 
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
 
-    public void onPause(){
-        //cameraView = null;
-    }
+    public void onPause(){}
 
     @Override
     public void onDestroy() {
@@ -159,9 +156,4 @@ public class RecordingService extends Service implements SurfaceHolder.Callback 
 
         super.onDestroy();
     }
-
-
-
-
 }
-
