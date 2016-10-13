75ad590e5b7b833366af19daf0066fc335590741
Matt Gerst <mattgerst@gmail.com>
2016-10-12 20:39:39 -0500

    Add API calls for transfering files.

---
+++ src/org/servalproject/api/Command.java
@@ -0,0 +1,144 @@
+package org.servalproject.api;
+
+import android.os.Bundle;
+
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Command {
+    private Map<String, byte[]> extras;
+    private String action;
+
+    public Command(String action) {
+        extras = new HashMap<>();
+        this.action = action;
+    }
+
+    /////////////
+    // Setters //
+    /////////////
+    public void putExtra(String key, byte[] value) {
+        extras.put(key, value);
+    }
+
+    public void putExtra(String key, String value) {
+        extras.put(key, value.getBytes());
+    }
+
+    public void putExtra(String key, int value) {
+        ByteBuffer b = ByteBuffer.allocate(4);
+        b.putInt(value);
+
+        extras.put(key, b.array());
+    }
+
+    public void putExtra(String key, long value) {
+        ByteBuffer b = ByteBuffer.allocate(8);
+        b.putLong(value);
+
+        extras.put(key, b.array());
+    }
+
+    /////////////
+    // Getters //
+    /////////////
+    public String getAction() {
+        return action;
+    }
+
+    public byte[] getExtra(String key) {
+        return extras.get(key);
+    }
+
+    public String getExtraString(String key) {
+        return new String(extras.get(key));
+    }
+
+    public int getExtraInt(String key) {
+        ByteBuffer buffer = ByteBuffer.wrap(extras.get(key));
+
+        return buffer.getInt();
+    }
+
+    public long getExtraLong(String key) {
+        ByteBuffer buffer = ByteBuffer.wrap(extras.get(key));
+
+        return buffer.getLong();
+    }
+
+    /////////////////////
+    // Format Shifting //
+    /////////////////////
+    public byte[] asBytes() {
+        int numBytes = 0;
+        byte[] act = action.getBytes();
+
+        numBytes += 4 + act.length; // size field plus length
+
+        numBytes += 4; // Extras count
+
+        int numExtras = 0;
+        for (Map.Entry<String, byte[]> entry : extras.entrySet()) {
+            numBytes += 4 + entry.getKey().getBytes().length;
+            numBytes += 4 + entry.getValue().length;
+
+            numExtras++;
+        }
+
+        ByteBuffer buffer = ByteBuffer.allocate(numBytes);
+        buffer.putInt(act.length);
+        buffer.put(act);
+
+        buffer.putInt(numExtras);
+
+        for (Map.Entry<String, byte[]> entry : extras.entrySet()) {
+            byte[] key = entry.getKey().getBytes();
+            byte[] value = entry.getValue();
+
+            buffer.putInt(key.length);
+            buffer.put(key);
+            buffer.putInt(value.length);
+            buffer.put(value);
+        }
+
+        return buffer.array();
+    }
+
+    public static Command parse(byte[] bytes) {
+        ByteBuffer buffer = ByteBuffer.wrap(bytes);
+        int actLen = buffer.getInt();
+
+        byte[] act = new byte[actLen];
+        buffer.get(act, 0, actLen);
+        String action = new String(act);
+
+        Command cmd = new Command(action);
+
+        int extraCount = buffer.getInt();
+        for (int i = 0; i < extraCount; i++) {
+            int keyLen = buffer.getInt();
+            byte[] keyArr = new byte[keyLen];
+            buffer.get(keyArr, 0, keyLen);
+            String key = new String(keyArr);
+
+            int valLen = buffer.getInt();
+            byte[] valArr = new byte[valLen];
+            buffer.get(valArr, 0, valLen);
+
+            cmd.putExtra(key, valArr);
+        }
+
+        return cmd;
+    }
+
+    public Bundle asBundle() {
+        Bundle b = new Bundle();
+
+        for (Map.Entry<String, byte[]> extra : extras.entrySet()) {
+            b.putByteArray(extra.getKey(), extra.getValue());
+        }
+
+        return b;
+    }
+}
--- src/org/servalproject/api/NetworkAPI.java
+++ src/org/servalproject/api/NetworkAPI.java
@@ -9,10 +9,16 @@ import org.servalproject.servaldna.AsyncResult;
 import org.servalproject.servaldna.ServalDCommand;
 import org.servalproject.servaldna.ServalDInterfaceException;
 import org.servalproject.servaldna.SubscriberId;
-import org.servalproject.servaldna.keyring.KeyringIdentity;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 
 /**
  * The entry point into the network api for sending messages across
@@ -31,9 +37,14 @@ public class NetworkAPI {
     public static final String MESH_RESP = MESH_CMD + ".response";
     public static final String MESH_START = MESH_CMD + ".start";
     public static final String MESH_END = MESH_CMD + ".end";
+    public static final String MESH_TRANSFER = MESH_CMD + ".transfer";
 
     public static final String CMD_DATA = "data";
     public static final String CMD_SRC = "source";
+    public static final String CMD_COMMAND = "cmd";
+
+    public static final int TCP_TRANSFER_PORT = 8193;
+    public static final int MDP_TRANSFER_PORT = 83;
 
     private NetworkAPI() {
         app = ServalBatPhoneApplication.context;
@@ -73,7 +84,13 @@ public class NetworkAPI {
                                 Log.d(TAG, "Message was a Start Command");
                                 intent.setAction(MESH_START);
                                 intent.putExtra(CMD_SRC, sid.toHex());
-                                intent.putExtra(CMD_DATA, payload);
+
+                                Command cmd = Command.parse(payload);
+                                if (cmd.getAction().equals(MESH_TRANSFER)) {
+                                    receiveFile(sid, cmd);
+                                }
+
+                                intent.putExtra(CMD_COMMAND, cmd.asBundle());
 
                                 app.sendBroadcast(intent);
                                 break;
@@ -107,46 +124,6 @@ public class NetworkAPI {
         return instance;
     }
 
-    /////////////
-    // Rhizome //
-    /////////////
-
-    /**
-     * WIP Implementation of sending a file.
-     *
-     * @param toSend The path of the file to send
-     * @return true on success, false otherwise
-     */
-    public boolean sendFile(File toSend) {
-        return sendFile(toSend, null);
-    }
-
-    /**
-     * WIP Implementation of sending a file.
-     *
-     * Note that there is no peer to send to, this is because
-     * rhizome puts the file on all devices in the mesh. We may
-     * want to accept the peer id here in case we don't use
-     * rhizome.
-     *
-     * @param toSend The path of the file to send
-     * @param manifest The path of the manifest
-     * @return true on success, false otherwise
-     */
-    public boolean sendFile(File toSend, File manifest) {
-        try {
-            KeyringIdentity identity = app.server.getIdentity();
-            ServalDCommand.rhizomeAddFile(toSend, manifest, null, identity.sid, null);
-            return true;
-        } catch (ServalDInterfaceException e) {
-            e.printStackTrace();
-            return false;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
     public void sendCommand(SubscriberId dst, byte[] data, CommandType type) {
         if (commandSocket == null) {
             initCommands();
@@ -164,6 +141,96 @@ public class NetworkAPI {
 
     public void sendResponse(SubscriberId dst, byte[] data) {
         sendCommand(dst, data, CommandType.RESPONSE);
+    }
+
+    public void sendStart(SubscriberId dst, Command cmd) {
+        sendCommand(dst, cmd.asBytes(), CommandType.START);
+    }
+
+    public void sendEnd(SubscriberId dst, String action) {
+        sendCommand(dst, action.getBytes(), CommandType.END);
+    }
+
+    public boolean sendFile(SubscriberId dst, File file) {
+        try {
+            Log.i(TAG, "Sending File");
+            Log.d(TAG, "Creating MSP Tunnel");
+            ServalDCommand.mspTunnnelCreate("servald", TCP_TRANSFER_PORT, MDP_TRANSFER_PORT);
+
+            Log.d(TAG, "Sending START Command");
+            Command cmd = new Command(MESH_TRANSFER);
+            cmd.putExtra("filename", file.getName());
+            cmd.putExtra("length", (int)file.length());
+            sendStart(dst, cmd);
+
+            Log.d(TAG, "Connecting to tunnel socket");
+            Socket tunneledSocket = new Socket("localhost", TCP_TRANSFER_PORT);
+            InputStream inStream = tunneledSocket.getInputStream();
+
+            Log.d(TAG, "Waiting for RDY command");
+            // Wait for the receiving socket to be ready
+            byte[] rdy = new byte[3];
+            int read = inStream.read(rdy, 0, 3);
+            if (read != 3 || !new String(rdy).toUpperCase().equals("RDY")) {
+                Log.e(TAG, "Got bad response from transfer client. Try again");
+
+                inStream.close();
+                tunneledSocket.close();
+                return false;
+            }
+
+            // Transfer the file
+            Log.d(TAG, "Starting file transfer");
+            OutputStream outStream = tunneledSocket.getOutputStream();
+            byte[] buffer = new byte[(int) file.length()];
+            FileInputStream fis = new FileInputStream(file);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+
+            // Read the file contents and put them into the socket
+            bis.read(buffer,0,buffer.length);
+            outStream.write(buffer,0,buffer.length);
+            outStream.flush();
+
+            bis.close();
+            fis.close();
+            tunneledSocket.close();
+            sendEnd(dst, MESH_TRANSFER);
+            Log.i(TAG, "Finished File Transfer");
+
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void receiveFile(SubscriberId frm, Command cmd) {
+        try {
+            Log.i(TAG, "Receiving File");
+            Log.d(TAG, "Opening Tunnel");
+            ServalDCommand.mspTunnelConnect("servald", TCP_TRANSFER_PORT, frm, MDP_TRANSFER_PORT);
+
+            Socket tunnelSocket = new Socket("localhost", TCP_TRANSFER_PORT);
+            tunnelSocket.getOutputStream().write("RDY".getBytes());
+
+            InputStream inStream = tunnelSocket.getInputStream();
+            byte[] buffer = new byte[cmd.getExtraInt("length")];
+            File file = new File(cmd.getExtraString("filename"));
+            FileOutputStream fos = new FileOutputStream(file);
+            BufferedOutputStream bos = new BufferedOutputStream(fos);
+
+            inStream.read(buffer, 0, buffer.length);
+            bos.write(buffer,0,buffer.length);
+            bos.flush();
+
+            bos.close();
+            fos.close();
+            inStream.close();
+            tunnelSocket.close();
+            Log.i(TAG, "Finished File Transfer");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public enum CommandType {
